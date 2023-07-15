08036436ac1681ab66ee0841e2c2f46ed686abf7
Ian <ianshaw93@gmail.com>
2023-07-15 19:09:49 +0100

    added categories and styling

--- app/components/Leaderboard.jsx
+++ app/components/Leaderboard.jsx
@@ -1,33 +1,93 @@
+"use client"
+import { useState } from 'react'
 import LeaderboardCard from './LeaderboardCard'
 
-export default function Leaderboard() {
-    const dummyLeaderboard = [{
-        "id": "0000", 
-        "name": "user1",
-        "personal_records": { // should be accessible in both lbs and kgs
-            "barbell_back_squat": 110,
-            "barbell_chest_press": 80,
-            "barbell_power_clean": 80
-        }
-    },
+const dummyLeaderboard = [
     {
-        "id": "0001", 
-        "name": "user2",
-        "personal_records": { 
-            "barbell_back_squat": 90,
-            "barbell_chest_press": 100,
-            "barbell_power_clean": null
-        }        
+    "id": "0000", 
+    "name": "user1",
+    "personal_records": { // should be accessible in both lbs and kgs
+        "barbell_back_squat": 110,
+        "barbell_chest_press": 80,
+        "barbell_power_clean": 80
+    }
+},
+{
+    "id": "0001", 
+    "name": "user2",
+    "personal_records": { 
+        "barbell_back_squat": 90,
+        "barbell_chest_press": 100,
+        "barbell_power_clean": null
+    }        
+}
+]
+export default function Leaderboard() {
+
+    const [selectedCategory, setSelectedCategory] = useState("barbell_back_squat")
+    const recordCategories = Object.keys(dummyLeaderboard[0]['personal_records'])
+
+    
+    const categoryDropdownContent = recordCategories.map((item) => {
+        return <option key={item} value={item}>{item}</option>
+    })
+
+    const categoryDropdown = (
+        <label className="min-w-full flex items-center justify-center">
+            <select
+            onChange={handleInput}
+            name='selectedCategory'
+            value={selectedCategory || ""}
+            className="w-9/12 text-center border p-2 mx-2 my-2"
+            >
+              { categoryDropdownContent }
+            </select>
+          </label>
+    )
+    // todo filter order for highest in particular lift
+
+    function handleInput(event) {
+        setSelectedCategory(event.target.value)
     }
 
-
-]
+// need to change on dropdown between pr's
     return (
       <>
+        <div className="form-control" >
+            {categoryDropdown}
+        </div>
       <div  className="min-h-screen bg-[#DED1BF]">
+        {/* Title  move to own component or function above*/}
+        <div className='grid-cols-3 flex justify-between max-w-full text-2xl font-bold'>
+            <div className='col-span-1 col-start-1 mx-5'>
+                Rank
+            </div>
+            <div className='col-span-1 col-start-2 text-left'>
+                Name
+            </div>
+            <div className='mx-5'>
+                Result
+            </div>
+        </div>
+        <ul>
+            {dummyLeaderboard.map((item, index) => {
+                return (
+                    <>
+                    <li>
+                        <LeaderboardCard 
+                            key={item.id}
+                            rank={index+1}
+                            name={item.name}
+                            weightLifted_kg={item.personal_records[selectedCategory]}
 
-        <LeaderboardCard />
-      {/* Leaderboard */}
+                        /> 
+
+                    </li>
+                    </>
+                )
+            })}
+        </ul>
+      {/* rank=1, name='Ian Shaw', weightLifted_kg=110, image=null */}
       </div>
       </>
     )
--- app/components/LeaderboardCard.jsx
+++ app/components/LeaderboardCard.jsx
@@ -13,20 +13,19 @@ export default function LeaderboardCard({rank=1, name='Ian Shaw', weightLifted_k
         {rank}
       </div>
       <div className="col-span-2 flex justify-between max-w-full" >
-        <div className="col-start-2 col-span-1 row-start-1 row-span-1" >
-        {/* LHS column: description, weight below*/}
-        <div className="w-11 rounded-full flex justify-center items-center">
-            <FontAwesomeIcon icon={faCircleUser} size='3x' color='grey'/>
-        </div>  
-        <div>
-          <h2 className="">{name}</h2>
-        </div>      
-          {/* <h2 className="">{weightLifted_kg}kg</h2> */}
+        <div className='flex items-center'>
+
+            {/* <div className="col-start-2 col-span-1 row-start-1 row-span-1" > */}
+            <div className="w-11 rounded-full flex justify-center items-center">
+                <FontAwesomeIcon icon={faCircleUser} size='3x' color='grey'/>
+            </div>  
+            <h2 className="mx-5 text-xl">{name}</h2>
+            {/* </div> */}
         </div>
         {/* RHS column: price, price/weight below*/}
         <div className="col-start-3 col-span-1 mt-3" >
         {/* <h2 className="">{name}</h2> */}
-          <h2 className="row-span-1 text-2xl text-right font-bold">{weightLifted_kg}kg</h2>
+          <h2 className="row-span-1 text-2xl text-right font-bold">{weightLifted_kg ? weightLifted_kg + "kg" : "n/a"}</h2>
           {/* <h2 className="row-span-1">£pricePerKiloRounded/kg</h2> */}
         </div>
       </div>
