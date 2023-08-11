edc6598cacce4db8c230c008011628c1d769ddd4
Ian <ianshaw93@gmail.com>
2023-08-11 09:15:53 +0100

    got sign in page working

--- app/sign-in/[[...sign-in]]/pages.jsx
+++
@@ -1,7 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  // not sure why doesn't work?
-  return <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />;
-  // <SignIn/> 
-}
