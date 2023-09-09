740372ebee95ccbd102604301422236f909a0597
Ian <ianshaw93@gmail.com>
2023-09-09 19:24:34 +0100

    text was white in dark mode, changed to explicit black

--- app/components/Tab.tsx
+++ app/components/Tab.tsx
@@ -10,7 +10,7 @@ const renderTabTriggers = (userGroups) => {
     return (
       <>
       <Tabs.Trigger
-        className="bg-white px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
+        className="bg-white px-5 h-[45px] text-black flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
         value={`tab-${key}`}
       >
         {currentGroup.groupName}
@@ -27,7 +27,7 @@ const renderTabContents = (userGroups, allPRs) => {
     return (
       <>
       <Tabs.Content
-        className="grow bg-white rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
+        className="grow bg-white text-black rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
         value={`tab-${key}`}
       >
         <Leaderboard allPRs={allPRs} groupMembers={currentGroup.userIds} groupName={currentGroup.groupName}/>
